2268b553d5c623c0d82ac5d1968c5fb145ae508e
Matthew Chick <c.chick@unsw.edu.au>
2019-09-13 13:18:17 +1000

    install axios and call it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';   //ES6 destructing
-import logo from './logo.svg';
-import './App.css';
+// axios - promise based http client, install it by yarn add axios
+import axios from 'axios'
 
 class App extends Component {   //React.Component
+  constructor(props) {
+    super(props)
+    //state
+    this.state = {    //state object
+      user: []
+    }
+  }
+
+  componentWillMount() {    //react component lifecycle
+    axios('https://api.randomuser.me/?nat=US&results=5')
+    .then(response => console.log(response));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello world
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
